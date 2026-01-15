5e669d4b540856ac1e1e925c59f364f4e5fd325d
lilianlacerda <lilianraiany@gmail.com>
2026-01-15 16:03:31 -0300

    feat: craição do StyledButton component

---
+++ src/components/StyledButton/StyledButton.tsx
@@ -0,0 +1,37 @@
+import { styled } from "@mui/material";
+import type { ReactNode } from "react";
+
+interface StyledButtonProps{
+    children: ReactNode
+}
+
+const MyCustomButton = styled("button")(({theme}) => ({
+    backgroundColor: "transparent",
+    border: `1px solid ${theme.palette.primary.contrastText}`,
+    borderRadius: "3px",
+    padding: "5px 15px",
+    width: "100%",
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "10px",
+    color: theme.palette.primary.contrastText,
+    '&:hover': {
+        backgroundColor: theme.palette.secondary.light
+    }
+        
+    }))
+
+const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
+
+    return(
+        <>
+            <MyCustomButton>
+                {children}
+            </MyCustomButton>
+
+        </>
+    )
+}
+
+export default StyledButton;
