d3afe2f3a893633d23b97605bcf6f6588d864704
lilianlacerda <lilianraiany@gmail.com>
2026-01-15 16:09:43 -0300

    feat: criação do Hero components UI

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import NavBar from "../../components/NavBar/NavBar";
+import Hero from "./Sections/Hero/Hero";
+
+const Home = () => {
+  return (
+        <>
+            <NavBar />
+            <Hero/>
+            
+        </>
+    )
+};
+
+export default Home;
