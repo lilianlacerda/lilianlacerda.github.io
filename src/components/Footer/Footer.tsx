2097becc4ce29de6bc477c972995d3304ac60ab2
lilianlacerda <lilianraiany@gmail.com>
2026-01-18 21:26:08 -0300

    feat: inicialização do footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,22 +2,36 @@ import { Box, Container, IconButton, Typography } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
+const HandleEmail = () => {
+    const emailAddress = 'lilianraiany@gmail.com';
+    const subject = 'Contato através do Portfólio';
+    const body = 'Olá! Vi seu portfólio e gostaria de conversar sobre uma oportunidade.'
+
+    const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+}
 
 const Footer = () => {
     return(
         <>
             <Box pt={2} pb={2}>
                 <Container maxWidth="sm">
-                    <Box display={"flex"} alignItems={"center"} justifyContent={"center"} gap={3} pb={1}>
+                    <Box display={"flex"} alignItems={"center"} justifyContent={"center"} gap={2} pb={1}>
                         <IconButton onClick={() => window.open("https://github.com/lilianlacerda")}>
                             <GitHubIcon />
                         </IconButton>
                         <IconButton onClick={() => window.open("https://www.linkedin.com/in/lilian-raiany-lacerda/")}>
                             <LinkedInIcon />
                         </IconButton>
+                        <IconButton onClick={HandleEmail}>
+                            <MailOutlineIcon />
+                        </IconButton>
                     </Box>
-                    <Typography>
-                        © 2026 Desenvolvido com <FavoriteIcon fontSize="small" sx={{color: "#e65100"}}/> por Lilian Lacerda - All rights reserved
+                    <Typography textAlign={"center"}>
+                        © 2026 Desenvolvido com <FavoriteIcon fontSize="small" sx={{color: "#FF8C00"}}/> por Lilian Lacerda - All rights reserved
                     </Typography>
                 </Container>
             </Box>
