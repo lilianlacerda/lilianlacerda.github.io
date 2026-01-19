d3b86d7738170b915591a0c21bd5b7f845f9b44a
lilianlacerda <lilianraiany@gmail.com>
2026-01-18 21:20:44 -0300

    feat: implementação do botão de baixar curriculo, contact via whatsapp e animação de subtitulo

--- src/pages/Home/Sections/Hero/Hero.tsx
+++ src/pages/Home/Sections/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import { Box, Container, Grid, styled, Typography } from "@mui/material";
 import Avatar from '../../../../assets/images/avatar.jpg'
 import DownloadIcon from '@mui/icons-material/Download';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
+import AnimatedSubTitle from "../../../../components/AnimatedSubTitle/AnimatedSubTitle";
+import meuCV from '../../../../assets/pdfs/cv_dev_Lilian_Lacerda.pdf';
 
 const StyledHero = styled("div")(({theme})=>({
         backgroundColor: theme.palette.primary.main,
@@ -24,6 +26,26 @@ const StyledImg = styled("img")(({theme})=>({
     border: `1px solid ${theme.palette.primary.contrastText}`
 }))
 
+const HandleWhatsApp = (): void => {
+    const phoneNumber = "5511970703567";
+    const message = "Olá! acessei seu portfólio e gostaria de conversar.";
+
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+    window.open(whatsappLink, "_blank");
+}
+
+const HandleDownload = (): void => {
+    const link = document.createElement('a');
+
+    link.href = meuCV;
+    link.download = "cv_dev_Lilian_Lacerda.pdf";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
 const Hero = () => {
 
     return(
@@ -43,20 +65,20 @@ const Hero = () => {
                         </Grid>
                         <Grid size={{ xs: 12, md: 7 }}>
                             <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Lilian Lacerda</Typography>
-                            <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
+                            <AnimatedSubTitle />
                             <Grid container display={"flex"} justifyContent={"center"} spacing={3} pt={3}>
                                 <Grid size={{xs: 12, md: 4}} display={"flex"} justifyContent={"center"}>
-                                    <StyledButton>
+                                    <StyledButton onClick={HandleDownload}>
                                         <DownloadIcon />
-                                        <Typography>
+                                        <Typography fontWeight={500}>
                                             Download CV
                                         </Typography>
                                     </StyledButton>
                                 </Grid>
                                 <Grid size={{xs: 12, md: 4}} display={"flex"} justifyContent={"center"}>
-                                    <StyledButton>
-                                        <MailOutlineIcon />
-                                        <Typography>
+                                    <StyledButton onClick={HandleWhatsApp}>
+                                        <WhatsAppIcon />
+                                        <Typography fontWeight={500}>
                                             Contact me
                                         </Typography>
                                     </StyledButton>
