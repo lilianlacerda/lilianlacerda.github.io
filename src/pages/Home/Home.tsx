fe16d016a2d363d190f7bfdb52987203e300b895
lilianlacerda <lilianraiany@gmail.com>
2026-01-22 19:23:38 -0300

    feat: inserir o projects na home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
 import Hero from "./Sections/Hero/Hero";
+import Projects from "./Sections/Projects/Projects";
 
 const Home = () => {
   return (
         <>
             <NavBar />
             <Hero/>
+            <Projects />
             <Footer />
             
         </>
