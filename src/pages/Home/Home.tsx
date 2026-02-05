6813cc01a37e2373dfdad66a27b834c8bc3d9a9d
lilianlacerda <lilianraiany@gmail.com>
2026-02-05 14:58:00 -0300

    feat: adicionando skills na home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
 import Hero from "./Sections/Hero/Hero";
 import Projects from "./Sections/Projects/Projects";
+import Skills from "./Sections/Skills/Skills";
 
 const Home = () => {
   return (
         <>
             <NavBar />
             <Hero/>
+            <Skills />
             <Projects />
             <Footer />
             
