f43b673e14fb309b77c4adc51cddb58d47cd01f2
lilianlacerda <lilianraiany@gmail.com>
2026-02-25 19:57:49 -0300

    style: mudanças de estilização

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, IconButton, Typography } from "@mui/material";
+import { Box, Container, IconButton, styled, Typography } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -14,28 +14,39 @@ const HandleEmail = () => {
     window.location.href = mailtoLink;
 }
 
+const StyledFooter = styled ("div")(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
+    color: "inherit",
+    cursor: "pointer",
+    borderTop: `0.5px solid ${theme.palette.divider}`,
+    boxShadow: "0px -10px 20px rgba(0,0,0,0.1)",
+    
+
+
+}))
+
 const Footer = () => {
     return(
-        <>
+        <StyledFooter>
             <Box pt={2} pb={2}>
                 <Container maxWidth="sm">
                     <Box display={"flex"} alignItems={"center"} justifyContent={"center"} gap={2} pb={1}>
                         <IconButton onClick={() => window.open("https://github.com/lilianlacerda")}>
-                            <GitHubIcon />
+                            <GitHubIcon sx={{ color: "primary.contrastText" }} />
                         </IconButton>
                         <IconButton onClick={() => window.open("https://www.linkedin.com/in/lilian-raiany-lacerda/")}>
-                            <LinkedInIcon />
+                            <LinkedInIcon sx={{ color: "primary.contrastText" }}/>
                         </IconButton>
                         <IconButton onClick={HandleEmail}>
-                            <MailOutlineIcon />
+                            <MailOutlineIcon sx={{ color: "primary.contrastText" }}/>
                         </IconButton>
                     </Box>
-                    <Typography textAlign={"center"}>
+                    <Typography textAlign={"center"} color="primary.contrastText">
                         © 2026 Desenvolvido com <FavoriteIcon fontSize="small" sx={{color: "#FF8C00"}}/> por Lilian Lacerda - All rights reserved
                     </Typography>
                 </Container>
             </Box>
-        </>
+        </StyledFooter>
     )
 
 }
