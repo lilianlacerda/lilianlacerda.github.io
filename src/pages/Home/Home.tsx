8eaf88781c0f4a65a83f589eefa7805fa38f9814
lilianlacerda <lilianraiany@gmail.com>
2026-02-25 19:58:46 -0300

    feat: adicionando about na home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
+import About from "./Sections/About/About";
 import Hero from "./Sections/Hero/Hero";
 import Projects from "./Sections/Projects/Projects";
 import Skills from "./Sections/Skills/Skills";
@@ -9,6 +10,7 @@ const Home = () => {
         <>
             <NavBar />
             <Hero/>
+            <About />
             <Skills />
             <Projects />
             <Footer />
