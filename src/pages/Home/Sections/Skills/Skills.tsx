44dfc013b7193e796054f65d86314a2bcc35645e
lilianlacerda <lilianraiany@gmail.com>
2026-02-25 19:56:00 -0300

    fix: melhorias de ajuste fino

--- src/pages/Home/Sections/Skills/Skills.tsx
+++ src/pages/Home/Sections/Skills/Skills.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, styled, Typography } from "@mui/material";
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
 import { hardSkills, softSkills } from "../../../../utils/iconsArray";
 import SkillsComponent from "../../../../components/SkillsComponent/SkillsComponent";
 
@@ -6,42 +6,35 @@ const StyledSkills = styled("div")(({theme})=>({
         backgroundColor: theme.palette.primary.main,
         minHeight: "100vh",
         display: "flex",
-        alignItems: "center",
+        alignItems: "flex-start",
 }))
 
 const Skills = () => {
   return (
     <>
     <StyledSkills>
-      <Container 
-            maxWidth="lg"
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                minHeight: "100vh",
-                pt: 8,
-                pb: 10,
-
-            }}
-      >
-        <Grid container spacing={4} id="skills">
+      <Container maxWidth="lg">
+        <Box pt={9} pb={3} mb={3} id="skills">
+            <Typography variant="h2" textAlign={"center"} color="primary.contrastText">Minhas Skills</Typography>
+        </Box>
+        <Grid container spacing={4} mt={10}>
           <Grid size={{xs: 12, md: 6}}>
-                <Typography variant="h2" color="primary.contrastText" textAlign={"center"}>HardSkills</Typography>
+                <Typography variant="h3" color="primary.contrastText" textAlign={"center"}>HardSkills</Typography>
 
-                <Grid container spacing={2} display={"flex"} justifyContent={"center"} pt={3}>
+                <Grid container spacing={2} justifyContent={"center"} pt={3}>
                     {hardSkills.map((skill, index) => (
-                    <Grid key={index} display="flex" justifyContent="center">
+                    <Grid key={index} justifyContent="center">
                         <SkillsComponent name={skill.name} icon={skill.icon} />
                     </Grid>
                     ))}
                 </Grid>
             </Grid>
             <Grid size={{xs: 12, md: 6}}>
-                <Typography variant="h2" color="primary.contrastText" textAlign={"center"}>SoftSkills</Typography>
+                <Typography variant="h3" color="primary.contrastText" textAlign={"center"}>SoftSkills</Typography>
 
-                <Grid container spacing={2} display={"flex"} justifyContent={"center"}pt={3}>
+                <Grid container spacing={2} justifyContent={"center"}pt={3}>
                     {softSkills.map((skill, index) => (
-                    <Grid key={index} display="flex" justifyContent="center">
+                    <Grid key={index} justifyContent="center">
                         <SkillsComponent name={skill.name} icon={skill.icon} />
                     </Grid>
                     ))}
